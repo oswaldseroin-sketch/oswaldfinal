626f5bd73112f1575dcd9b0a676c4122ea0b89fb
oswaldseroin-sketch <oswaldseroin@gmail.com>
2026-09-01 10:06:45 +0300

    Updated BestDuoGame.tsx

--- src/components/games/BestDuoGame.tsx
+++ src/components/games/BestDuoGame.tsx
@@ -164,7 +164,7 @@ export default function BestDuoGame({ onBack, onProfileUpdate }: Props) {
 >
   {players[1]}
 </p>
-    {votes === undefined && (
+    
   <div
     className={`flex h-6 w-6 items-center justify-center rounded-full border transition-all duration-200 ${
       isSelected || isChosen
@@ -182,7 +182,7 @@ export default function BestDuoGame({ onBack, onProfileUpdate }: Props) {
       <Check size={13} />
     ) : null}
   </div>
-)}
+
     </button>
   )
 
