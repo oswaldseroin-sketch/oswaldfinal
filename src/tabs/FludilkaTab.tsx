9c4b17943136b761f596e3dd41a00af338c22807
oswaldseroin-sketch <oswaldseroin@gmail.com>
2026-09-04 14:48:51 +0300

    Updated FludilkaTab.tsx

--- src/tabs/FludilkaTab.tsx
+++ src/tabs/FludilkaTab.tsx
@@ -6,6 +6,12 @@ import {
 } from '../lib/fludilka'
 import { todayKey } from '../lib/storage'
 import { api } from '../lib/api'
+type SpecialEmoji = {
+  id: number
+  name: string
+  image_url: string
+  locked: boolean
+}
 const SERVER_URL = 'http://201.24.54.238:3001'
 
 function formatTime(iso: string): string {
