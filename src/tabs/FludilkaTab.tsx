009e80d31b8387f7308015b57186ee02740d1f64
oswaldseroin-sketch <oswaldseroin@gmail.com>
2026-09-04 15:50:06 +0300

    Updated FludilkaTab.tsx

--- src/tabs/FludilkaTab.tsx
+++ src/tabs/FludilkaTab.tsx
@@ -41,7 +41,7 @@ function renderMessage(
    ${message.trim() === `[emoji:${emoji.id}]` ? 'animate-emojiPop' : ''}
     ${
       message.trim() === `[emoji:${emoji.id}]`
-        ? 'h-24 w-24'
+        ? 'h-20 w-20'
         : 'h-10 w-10'
     }
   `}
