962ca8df826c4245871ef33ad274b16722f8a938
oswaldseroin-sketch <oswaldseroin@gmail.com>
2026-09-05 18:22:09 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+
 
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-  registerType: 'prompt',
-
-  manifest: {
-        name: 'Амальгама',
-        short_name: 'Амальгама',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#0d0e12',
-        theme_color: '#0d0e12',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-
-     workbox: {
-  cleanupOutdatedCaches: true,
-},
-    }),
+    
   ],
 
   server: {
