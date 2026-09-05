47c35b8a313dc1e8c06d34d7b5e5bf126cc94245
oswaldseroin-sketch <oswaldseroin@gmail.com>
2026-09-05 09:13:24 +0300

    Updated MiniGamesPanel.tsx

--- src/components/MiniGamesPanel.tsx
+++ src/components/MiniGamesPanel.tsx
@@ -682,7 +682,7 @@ const style = gameStyle[game.number]
               </button>
             )
           })}
-          </div>
+         
         <div className="mt-5 flex justify-center">
   <button
     onClick={onBack}
